2aa21708c14e5d75020fed3c5adbe8d49f8ffbb2
Jonayed Hossan Gazi <me@zuna.id>
2024-07-07 01:00:27 +0600

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,14 @@
 
 require_once 'vendor/autoload.php';
 
-
+try{
 $db = new MysqliDb ('host', 'username', 'password', 'databaseName');
+}catch($e){
+var_dump($e);
+}
+try{
 var_dump(new phptestpack\TestClass);
-
+}catch($e){
+var_dump($e);
+}
+var_dump(class_exists("phptestpack\TestClass"), get_declared_classes());
