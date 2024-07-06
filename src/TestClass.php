bf09e0d70880e7df36ffaf19ceedcdc89f2abc9a
Jonayed Hossan Gazi <me@zuna.id>
2024-07-07 00:54:49 +0600

    Update TestClass.php

--- src/TestClass.php
+++ src/TestClass.php
@@ -1,15 +1,12 @@
 <?
-namespace phpTestPack{
-use MysqliDb;
-class TestClass {
+namespace phpTestPack;
+
+public class TestClass {
+
 public function __construct(){
-print("heyy you");
+  print("heyy you");
 }
   public function doTest(){
     echo __CLASS__, time(), "Ok";
   }
-}
-  $db = new MysqliDb ('host', 'username', 'password', 'databaseName');
-  var_dump($db);
-  
 }
